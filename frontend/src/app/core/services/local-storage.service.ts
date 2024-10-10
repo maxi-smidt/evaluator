5b7317a79928948b4d2fc3ff9b7e5769c4e0858f
maximiliansmidt <maxismidt@icloud.com>
2024-10-10 14:06:58 +0200

    [Bug #71] going back from CourseInstance Settings

---
+++ frontend/src/app/core/services/local-storage.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  setItem<T>(key: string, value: T): void {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  }
+
+  getItem<T>(key: string): T | null {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
