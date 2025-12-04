d7c9eb5a957754a3cefb647fdfd2f5556d799568
msmidt <maxismidt@icloud.com>
2025-12-04 22:19:17 +0100

    [Feature #189] ci/cd workflow

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// just a test
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
