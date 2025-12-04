aa173a9030d29fcdfc084ec12264f825f0b2a9bf
msmidt <maxismidt@icloud.com>
2025-12-04 23:49:59 +0100

    [Feature #189] ci/cd workflow

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+set -e
+
+echo "--- Stopping and removing existing containers ---"
+docker-compose down
+
+echo "--- Pulling latest code from Git ---"
+git pull
+
+echo "--- Pulling latest Docker images defined in docker-compose.yml ---"
+docker-compose pull
+
+echo "--- Starting new containers in detached mode ---"
+docker-compose up -d
+
+echo "Deployment complete!"
