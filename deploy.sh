44d03082e88d01366e1d073ddd5037b437de1405
msmidt <maxismidt@icloud.com>
2025-12-10 14:02:39 +0100

    [NOISSUE] quickfix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,13 @@ docker-compose down
 echo "--- Pulling latest Docker images defined in docker-compose.yml ---"
 docker-compose pull
 
-echo "--- Apply Django migration ---"
-docker-compose exec backend python manage.py migrate
-
 echo "--- Starting new containers in detached mode ---"
 docker-compose up -d
+
+echo "--- Waiting for Database to initialize ---"
+sleep 10
+
+echo "--- Apply Django migration ---"
+docker-compose exec backend python manage.py migrate
 
 echo "Deployment complete!"
