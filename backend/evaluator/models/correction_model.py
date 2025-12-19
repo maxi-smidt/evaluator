ecfcca688cfec7b3ad5a6fb2db8591e99f732a24
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2025-12-19 20:39:42 +0100

    [Bug #35] late submission penalty persistance (#240)

--- backend/evaluator/models/correction_model.py
+++ backend/evaluator/models/correction_model.py
@@ -34,6 +34,17 @@ class Correction(models.Model):
     draft = models.JSONField(null=True)
     late_submitted_days = models.IntegerField(null=False,
                                               default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    late_submission_penalty = models.DecimalField(decimal_places=3,
+                                                  max_digits=6,
+                                                  null=True,
+                                                  default=None)
+
+    def save(self, *args, **kwargs):
+        if self.late_submission_penalty is None:
+            self.late_submission_penalty = self.assignment_instance.course_instance.late_submission_penalty
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.student} - {self.assignment_instance} - {self.tutor} - {self.status}"
