5eab207f9ad2d2e2af858aa60d9bf5ba5bbca7d6
msmidt <maxismidt@icloud.com>
2025-12-23 19:39:57 +0100

    home migration

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,51 +1,34 @@
-import { Component, inject, OnInit, Type } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { UserService } from '../../core/services/user.service';
-import { Role, User } from '../../core/models/user.models';
+import { Role } from '../../core/models/user.models';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
 import { Message } from 'primeng/message';
 import { NgComponentOutlet } from '@angular/common';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { DpdHomeComponent } from './dpd-home/dpd-home.component';
 import { TutorHomeComponent } from './tutor-home/tutor-home.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ms-home',
   templateUrl: './home.component.html',
   imports: [TranslatePipe, Message, NgComponentOutlet],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   private userService = inject(UserService);
 
-  protected homeComponent?: Type<
-    DpdHomeComponent | AdminHomeComponent | TutorHomeComponent
-  >;
-  protected user?: User;
-
-  public ngOnInit() {
-    this.userService.getUser().subscribe({
-      next: (value) => {
-        this.user = value;
-        this.loadHomeComponent().then();
-      },
-    });
-  }
-
-  private async loadHomeComponent() {
-    switch (this.user?.role) {
-      case Role.TUTOR: {
-        this.homeComponent = TutorHomeComponent;
-        break;
-      }
-      case Role.DEGREE_PROGRAM_DIRECTOR: {
-        this.homeComponent = DpdHomeComponent;
-        break;
-      }
-      case Role.ADMIN: {
-        this.homeComponent = AdminHomeComponent;
-        break;
-      }
+  protected user = toSignal(this.userService.getUser());
+  protected homeComponent = computed(() => {
+    if (!this.user()) return undefined;
+    switch (this.user()!.role) {
+      case Role.TUTOR:
+        return TutorHomeComponent;
+      case Role.DEGREE_PROGRAM_DIRECTOR:
+        return DpdHomeComponent;
+      case Role.ADMIN:
+        return AdminHomeComponent;
       default:
-        this.homeComponent = undefined;
+        return undefined;
     }
-  }
+  });
 }
