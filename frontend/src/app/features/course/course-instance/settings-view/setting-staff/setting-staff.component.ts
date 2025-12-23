a94e1348de45c4712152ce07cdb1335ed35b2965
msmidt <maxismidt@icloud.com>
2025-12-23 21:39:44 +0100

    setting staff migration

--- frontend/src/app/features/course/course-instance/settings-view/setting-staff/setting-staff.component.ts
+++ frontend/src/app/features/course/course-instance/settings-view/setting-staff/setting-staff.component.ts
@@ -1,53 +1,45 @@
-import { Component, input, model, OnInit } from '@angular/core';
+import { Component, computed, inject, input, model } from '@angular/core';
 import { Role, User } from '../../../../../core/models/user.models';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../../../core/services/user.service';
 import { StaffControlComponent } from './staff-control/staff-control.component';
-import { TranslationService } from '../../../../../shared/services/translation.service';
+import { TranslatePipe } from '../../../../../shared/pipes/translate.pipe';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'ms-edit-staff',
-  imports: [MultiSelectModule, FormsModule, StaffControlComponent],
+  imports: [
+    MultiSelectModule,
+    FormsModule,
+    StaffControlComponent,
+    TranslatePipe,
+  ],
   templateUrl: './setting-staff.component.html',
 })
-export class SettingStaffComponent implements OnInit {
-  user = input.required<User>();
-  courseInstanceId = input.required<number>();
+export class SettingStaffComponent {
+  private readonly userService = inject(UserService);
 
-  selectableCourseLeaders: User[] = [];
-  selectedCourseLeaders = model.required<User[]>();
+  public user = input.required<User>();
+  public courseInstanceId = input.required<number>();
+  public selectedCourseLeaders = model.required<User[]>();
+  public selectedTutors = model.required<User[]>();
 
-  selectableTutors: User[] = [];
-  selectedTutors = model.required<User[]>();
+  private users = toSignal(
+    toObservable(this.courseInstanceId).pipe(
+      switchMap((id) =>
+        this.userService.getUsers([`course=${id}`, 'get-dp=true']),
+      ),
+    ),
+  );
 
-  constructor(
-    private userService: UserService,
-    private translationService: TranslationService,
-  ) {}
-
-  ngOnInit() {
-    if (this.user().role === Role.TUTOR) {
-      return;
-    }
-
-    this.userService
-      .getUsers([`course=${this.courseInstanceId()}`, 'get-dp=true'])
-      .subscribe({
-        next: (value) => {
-          this.pushUsersSeparated(value);
-        },
-      });
-  }
-
-  pushUsersSeparated(users: User[]) {
-    this.selectableTutors = users.filter((u) => u.role === Role.TUTOR);
-    this.selectableCourseLeaders = users.filter(
-      (u) => u.role === Role.COURSE_LEADER,
-    );
-  }
-
-  translate(key: string) {
-    return this.translationService.translate(key);
-  }
+  protected selectableCourseLeaders = computed(() =>
+    this.users() ? this.users()!.filter((u) => u.role === Role.TUTOR) : [],
+  );
+  protected selectableTutors = computed(() =>
+    this.users()
+      ? this.users()!.filter((u) => u.role === Role.COURSE_LEADER)
+      : [],
+  );
 }
